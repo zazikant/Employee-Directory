36102c16ae0fe95c64fab2858aeeac76282c146c
Shashikant Zarekar <33363844+zazikant@users.noreply.github.com>
2025-11-05 17:11:08 +0530

    df

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    domains: ['maqvnokqutuedmijohqe.supabase.co'],
+  },
 };
 
 export default nextConfig;
