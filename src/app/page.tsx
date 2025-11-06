845dc152e3c7f7080725ab0ecb588cc5fda6c198
Shashikant Zarekar <33363844+zazikant@users.noreply.github.com>
2025-11-06 15:30:41 +0530

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,19 @@ export default function Home() {
           This website is the official employee data of GEM Engserv. We value their contribution in taking GEM to a new height.
         </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <Link href="/directory" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-primary focus:text-primary">
+        <div className="my-12">
+          <h2 className="text-4xl font-bold mb-6 text-primary">Our Values</h2>
+          <ol className="text-xl list-decimal list-inside text-left inline-block">
+            <li>Customer Focus</li>
+            <li>Ownership</li>
+            <li>Integrity</li>
+            <li>Value Creation Through Competence</li>
+            <li>Respect for Individual</li>
+          </ol>
+        </div>
+
+        <div className="flex flex-wrap items-center justify-around max-w-4xl sm:w-full">
+          <Link href="/directory" className="p-6 text-left border w-96 rounded-xl hover:text-primary focus:text-primary">
             <h3 className="text-2xl font-bold">Explore Our GEMians &rarr;</h3>
             <p className="mt-4 text-xl">
               Meet the incredible people who make GEM a great place to be.
