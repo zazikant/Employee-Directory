bedcddda865b19cd236add389c73df43dfca5b55
Shashikant Zarekar <33363844+zazikant@users.noreply.github.com>
2025-11-07 14:40:29 +0530

    ss

--- src/components/EmployeeCard.tsx
+++ src/components/EmployeeCard.tsx
@@ -1,11 +1,23 @@
+"use client";
 import Link from 'next/link';
 import Image from 'next/image';
 import { Employee } from '@/types';
+import { useState } from 'react';
 
 export default function EmployeeCard({ employee }: { employee: Employee }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
-    <div className="group perspective-1000">
-      <div className="relative w-64 h-64 transform-style-3d transition-transform duration-700 group-hover:rotate-y-180">
+    <div className="group perspective-1000" onClick={handleFlip}>
+      <div
+        className={`relative w-64 h-64 transform-style-3d transition-transform duration-700 ${
+          isFlipped ? 'rotate-y-180' : ''
+        } group-hover:rotate-y-180`}
+      >
         {/* Front of the card */}
         <div className="absolute w-full h-full backface-hidden">
           <Image
