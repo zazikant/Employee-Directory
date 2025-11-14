a4d566b88fbeacc9efba457442d1539c4b9d385e
Shashikant Zarekar <33363844+zazikant@users.noreply.github.com>
2025-11-14 16:56:47 +0530

    Delete src/src directory

--- src/src/utils/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://maqvnokqutuedmijohqe.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hcXZub2txdXR1ZWRtaWpvaHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzMTc3NDEsImV4cCI6MjA3Nzg5Mzc0MX0.Kf0jdhhVzYCWKO8ULi8nLa7B41OajBY64-BZ3DKnO6k'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
